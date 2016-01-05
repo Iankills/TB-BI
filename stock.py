166660fc16359377a97b800b3cf34e9f2f10da28
Bauridar <joss.14@hotmail.fr>
2016-01-05 13:05:53 +0100

    dfsdf

--- stock.py
+++ stock.py
@@ -6,4 +6,10 @@ import csv
 fname = "WalletTransactions.csv"
 file = open(fname, "rb")
 
+reader = csv.reader(file)
+for line in reader:
+	p = 1
+	i=0
+	unitP = 0
+	vararecup = line[0].split(' ', 1 )
 
