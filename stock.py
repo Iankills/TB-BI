8f5551bc0187495c9545121475031124720466f7
Iankills <pierre.emprou@gmail.com>
2016-01-05 13:15:41 +0100

    maj

--- stock.py
+++ stock.py
@@ -7,9 +7,13 @@ fname = "WalletTransactions.csv"
 file = open(fname, "rb")
 
 reader = csv.reader(file)
+
+#
+
 for line in reader:
 	p = 1
 	i=0
 	unitP = 0
 	vararecup = line[0].split(' ', 1 )
+	print line[3]
 
