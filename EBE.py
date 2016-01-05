45b4228f5a045ded670957b3592be9a5ff624c59
Bauridar <joss.14@hotmail.fr>
2016-01-05 13:14:40 +0100

    dfsdf

--- EBE.py
+++ EBE.py
@@ -1,3 +1,14 @@
 import csv;
 
-trgrt
+fname = "WalletTransactions.csv"
+file = open(fname, "rb")
+from io import StringIO
+csvf = StringIO(file.read().decode())
+reader = csv.reader(csvf,delimiter=',')
+for line in reader:
+	p = 1
+	i=0
+	unitP = 0
+	vararecup = line[0].split(' ', 1 )
+
+file.close()
